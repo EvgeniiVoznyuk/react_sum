a223b4606a0748b0817ed9fd5749ce165d27d0c6
Evgenii <voznukzhenia@gmail.com>
2020-10-21 15:03:24 +0300

    Updates

---
+++ precache-manifest.0f3fba8205e4240b89fa563ac7244038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86f712153d226c5e78850b4c53e27fa8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f899093cce46f29e6e23",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9cb276670027b79266c7",
+    "url": "./static/js/2.bafbf7af.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.bafbf7af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f899093cce46f29e6e23",
+    "url": "./static/js/main.ffe95d38.chunk.js"
+  },
+  {
+    "revision": "c2acbe53827daed63a5f",
+    "url": "./static/js/runtime-main.1aec85e4.js"
+  }
+]);
